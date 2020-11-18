e64e6d45cd637bdf48b1263e460b5a37a3314d74
christinezosche <christine.zosche@gmail.com>
2020-11-18 08:37:22 -0800

    commit tree 52c824c61be5804094b9fddcdfeb0b8b11b11592

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let bowlWin = array.find( function(object)
+  { return object.result === "W"
+  });
+  if (bowlWin != undefined) {
+  return bowlWin.year }
+  else { return undefined }
+};
